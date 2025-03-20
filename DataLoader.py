26be8ab6f95c3501309ba0cc565f9218e389f623
Efe Yencilek <64659675+FarukEfe@users.noreply.github.com>
2025-03-20 18:27:26 -0400

    feature: write dataloader (untested)

--- DataLoader.py
+++ DataLoader.py
@@ -1,7 +1,26 @@
 import pandas as pd
+import os
+
+from Graphs import *
 
 class DataLoader:
     
     def __init__(self, path:str):
-        table = pd.read_csv(path)
+        self.connections = pd.read_csv(os.path.join(path, 'london_connections.csv'))
+        self.stations = pd.read_csv(os.path.join(path, 'london_stations.csv'))
+    
+    def graph(self) -> Graph:
+        n_node = max(self.stations['id'])
+        _connection: pd.DataFrame = self.connections[['station1', 'station2', 'time']]
+        obj = WeightedGraph(n_node)
+        _connection.itertuples()
+        
+        for i in range(len(_connection)):
+            row = _connection.iloc[i]
+            p, q, w = row['station1'], row['station2'], row['time']
+            obj.add_edge(p, q, w)
+        
+        return obj
+
+
         
